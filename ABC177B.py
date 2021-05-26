126dc0a16401262c9d87a9cf1a7ba56910e8a547
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-05-26 11:09:04 +0900

    Add files via upload

---
+++ ABC177B.py
@@ -0,0 +1,15 @@
+s = input()
+t = input()
+
+ans = 1001
+
+for i in range(len(s) - len(t) + 1):
+  score = 0
+  u = s[i:i + len(t)]
+  for j in range(len(t)):
+    if u[j] != t[j]:
+      score += 1
+  ans = min(ans,score)
+
+print(ans)
+
