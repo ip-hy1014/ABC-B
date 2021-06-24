0baceb0352ed92be14db83ab7c3f7e59cc209922
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-06-24 22:51:58 +0900

    Add files via upload

---
+++ ABC121B.py
@@ -0,0 +1,8 @@
+n,m,c = map(int, input().split())
+b = list(map(int, input().split()))
+ans = 0
+for i in range(n):
+  a = list(map(int, input().split()))
+  if sum(ai*bi for ai,bi in zip(a,b))+c > 0:
+    ans += 1
+print(ans)
