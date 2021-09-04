9f09eb287955378768677933cc83758591e0b564
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-09-05 00:11:44 +0900

    Update ABC217B.py

--- ABC217B.py
+++ ABC217B.py
@@ -4,3 +4,11 @@ for i in range(3):
 l = ["ABC","ARC","AGC","AHC"]
 ans = list(set(s)^set(l))
 print(ans[0])
+
+#別解
+s = []
+for i in range(3):
+  s.append(input())
+l = ["ABC","ARC","AGC","AHC"]
+ans = set(s)^set(l)
+print(*ans)
