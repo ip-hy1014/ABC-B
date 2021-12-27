b8c863913e5a2a37311e4100db23ba5bc93c62b1
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-12-28 00:37:57 +0900

    Update ABC093B.py

--- ABC093B.py
+++ ABC093B.py
@@ -7,3 +7,16 @@ if b-a+1>2*k:
 else:
     for i in range(a,b+1):
       print(i)
+
+#別解
+a,b,k = map(int,input().split())
+ans = set()
+for i in range(a,a+k):
+  if a<=i<=b:
+    ans.add(i)
+for i in range(b,b-k,-1)[::-1]:
+  if a<=i<=b:
+    ans.add(i)
+ans = sorted(list(ans))
+for i in ans:
+  print(i)
