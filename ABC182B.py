ce4889226b8989df6f442b95ec333aaf238def67
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-01-07 00:38:50 +0900

    Update ABC182B.py

--- ABC182B.py
+++ ABC182B.py
@@ -1,23 +1,13 @@
-"""
-問題文
-数列 A (A1,A2,A3,…,AN) が与えられます。
-正の整数 k の GCD 度を、A1,A2,A3,…,AN のうち k で割り切れるものの数と定義します。
-2 以上の整数のうち GCD 度が最大になるものを一つ求めてください。 GCD 度が最大のものが複数ある場合どれを出力しても構いません。
-"""
-
 n = int(input())
-a = list(map(int, input().split()))
-
+a = list(map(int,input().split()))
 ans = 0
 mx = 0
-
-for x in range(2,1001):
-  score = 0
-  for b in a:
-    if b % x == 0:
-      score += 1
-  if score > mx:
-    ans = x
-    mx = score
-
+for i in range(2,1001):
+  c = 0
+  for j in a:
+    if j%i==0:
+      c+=1
+  if c>mx:
+    ans = i
+    mx = c
 print(ans)
