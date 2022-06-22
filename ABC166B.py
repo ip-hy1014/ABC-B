b4ae7c3d2adcf8a4ab7437db6b80ac8359e05576
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-06-22 15:04:25 +0900

    Update ABC166B.py

--- ABC166B.py
+++ ABC166B.py
@@ -8,3 +8,20 @@ for i in range(k):
 s = set(l)
 sl = len(s)
 print(n-sl)
+
+#別解
+import itertools
+from collections import Counter
+n,k = map(int,input().split())
+l = []
+for i in range(k):
+  d = int(input())
+  a = list(map(int,input().split()))
+  l.append(a)
+l = list(itertools.chain.from_iterable(l))
+c = Counter(l)
+ans = 0
+r = []
+for j in c.keys():
+  r.append(j)
+print(n-len(r))
